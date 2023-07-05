f127085c2c800b1ac549e528406b14688d5979de
edcastillob <edwar.castillo@gmail.com>
2023-07-05 14:26:08 -0400

    ENDPOINT

--- src/endpoint/ENDPOINT.js
+++ src/endpoint/ENDPOINT.js
@@ -1,2 +1,4 @@
-export const ENDPOINT = "http://localhost:3001/countries";
-export const ENDPOINT2 = "http://localhost:3001/activities";
+// export const ENDPOINT = "http://localhost:3001/countries";
+// export const ENDPOINT2 = "http://localhost:3001/activities";
+export const ENDPOINT = "https://backendcountries.onrender.com/countries";
+export const ENDPOINT2 = "https://backendcountries.onrender.com/activities";
